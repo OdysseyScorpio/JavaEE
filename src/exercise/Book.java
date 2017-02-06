32dd5c1ad7aea114c5162b2c835dc2f3c11eb4cd
Martyn Green <martyn.robert.green@gmail.com>
2017-02-06 15:10:10 +0000

    my first commit

---
+++ src/exercise/Book.java
@@ -0,0 +1,46 @@
+package exercise;
+
+public class Book {
+private int id;
+private String title;
+private int numberOfPages;
+	public Book (int id, String title, int numberOfPages){
+		
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the numberOfPages
+	 */
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+	/**
+	 * @param numberOfPages the numberOfPages to set
+	 */
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+}

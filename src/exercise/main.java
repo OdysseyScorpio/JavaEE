096731c8643047642e464e68fcd67d4a7e06f6dc
Martyn Green <martyn.robert.green@gmail.com>
2017-02-06 15:55:55 +0000

    added functionallity

--- src/exercise/main.java
+++ src/exercise/main.java
@@ -1,10 +1,13 @@
 package exercise;
 
+
+
 public class main {
 
 	public static void main(String[] args) {
 
-		
+		Book newBook = new Book(1, "Mody Dick", 950);
+		service.addToMap(newBook);
 		
 	}
 
--- src/exercise/service.java
+++ src/exercise/service.java
@@ -1,8 +1,17 @@
 package exercise;
 
-public class service {
+import java.util.HashMap;
+import java.util.Map;
 
-	public void addToMap(){
-		
+public class service {
+	static Map<String, Book> bookStore = new HashMap<>();
+
+	public static void addToMap(Book book) {
+		bookStore.put(book.getTitle(), book);
 	}
+
+	public static void deleteFromMap(String title, Book book) {
+		bookStore.remove(title, book);
+	}
+
 }
